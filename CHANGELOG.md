e92f678168ce3b0674e0ff3c8511086ecaf4f63b
clxrityy <97744702+clxrityy@users.noreply.github.com>
2023-12-17 00:19:51 -0500

    fix

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # @clxrity/sample-pkg
 
+## 0.2.0
+
+### Minor Changes
+
+- test
+
 ## 0.1.0
 
 ### Minor Changes
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export const sample = () => {
-    console.log("sample-pkg");
+    console.log("sample-pkgg");
 };
